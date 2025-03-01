bbe7e092a8b0aa014e1caff1a3f3733e63d1c9c2
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-01 13:22:46 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 // src/components/Footer.tsx
 import React from 'react';
-import '../styles.css'; // Import styles nếu footer dùng CSS từ styles.css
+import '../styles.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <p>© 2025 CaslaQuartz. All rights reserved.</p>
-      <p>Ứng dụng được phát triển bởi TDNM</a></p>
+      <p>Ứng dụng được phát triển bởi <a href="https://tdn-m.com" target="_blank" rel="noopener noreferrer">TDNM</a></p>
     </footer>
   );
 };
