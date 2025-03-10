255ce32017b623048d4e5842d98a6dffa1e3647c
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-10 23:31:08 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { fabric } from 'fabric';
+import * as fabric from 'fabric';
 import Footer from './components/Footer';
 import UsageGuide from './components/UsageGuide';
 
